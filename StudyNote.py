a372ce58fd5b94fcd1ae87346353767ba1ff19d4
irook <koorikun@foxmail.com>
2017-10-28 22:25:56 +0800

    :>
    ;>

--- StudyNote.py
+++ StudyNote.py
@@ -10,24 +10,40 @@ app.config['SECRET_KEY'] = os.urandom(16)
 notes = []
 DATABASE = 'note.db'
 
+
 # 获取在 SQLite 中的数据
 def get_note():
     new_notes = []
     cur = g.db.execute('SELECT * FROM Notes')
     for row in cur.fetchall():
-        new_notes.append(Note(row[0], row[1]))
+        new_notes.append(Note(row[0], markdown.markdown(row[1])))
     return new_notes
+
+
+# 获取在 SQLite 中原始数据
+def get_raw_note(name):
+    cur = g.db.execute('SELECT * FROM Notes WHERE note_title = ?', (name,))
+    got_note = cur.fetchall()
+
+    if len(got_note) != 0:
+        got_note = got_note[0]
+    else:
+        return False
+
+    return Note(got_note[0], got_note[1])
 
 
 # 获取单一的 Note
 def get_single_note(name):
     cur = g.db.execute('SELECT * FROM Notes WHERE note_title = ?', (name,))
-    try:
-        got_note = cur.fetchall()[0]
-    except():
-        return None
+    got_note = cur.fetchall()
 
-    return Note(got_note[0], got_note[1])
+    if len(got_note) != 0:
+        got_note = got_note[0]
+    else:
+        return False
+
+    return Note(got_note[0], markdown.markdown(got_note[1]))
 
 
 # 往 SQLite 中插入 Note
@@ -76,12 +92,14 @@ def note():
         passage = request.form['passage']
 
         if title not in notes:
-            new_note = Note(title, markdown.markdown(passage))
+            new_note = Note(title, passage)
             append_note(new_note)
             notes.append(new_note)
+
         else:
             flash('Title already exists!')
             redirect(url_for('note'))
+    notes = get_note()
     return render_template('note.html', notes=reversed(notes))
 
 
@@ -90,7 +108,7 @@ def note():
 def notepage(name):
     if request.method == 'GET':
         single_note = get_single_note(name)
-        if single_note is not None:
+        if single_note is not False:
             return render_template('notepage.html', note=single_note)
         else:
             abort(404)
@@ -98,6 +116,23 @@ def notepage(name):
     elif request.method == 'POST':
         notename = request.form['DELETE']
         delete_note(notename)
+        return redirect(url_for('note'))
+
+
+# 编辑Note
+@app.route('/note/edit/<name>', methods=['GET', 'POST'])
+def editnote(name):
+    if request.method == 'GET':
+        single_note = get_raw_note(name)
+        if single_note is not False:
+            return render_template('noteedit.html', note=single_note)
+        else:
+            abort(404)
+    elif request.method == 'POST':
+        notename = request.form['name']
+        notepassage = request.form['passage']
+        delete_note(notename)
+        append_note(Note(notename, notepassage))
         return redirect(url_for('note'))
 
 
